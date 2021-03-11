9adf88c72ef6ab0d8c8fe556c990078d48fdc818
Sergey L <yo-suke@mail.ru>
2021-03-11 12:03:20 +0300

    fix maintainability

--- src/games/gameBrainEven.js
+++ src/games/gameBrainEven.js
@@ -3,12 +3,12 @@ import getRandomNumber from '../util.js';
 
 const gameRule = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-const checkEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const checkEven = (number) => number % 2 === 0;
 
 const runGameRound = () => {
   const number = getRandomNumber(0, 101);
   const question = number;
-  const answer = checkEven(number);
+  const answer = checkEven(number) ? 'yes' : 'no';
   const result = [question, answer];
   return result;
 };
